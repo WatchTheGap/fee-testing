4bdb5a73b3ad520b06405dc1a962b0d133f3a79c
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-15 16:40:49 -0400

    passed test for no arguments - default total is set to zero

--- test/specs/calc.spec.js
+++ test/specs/calc.spec.js
@@ -8,8 +8,13 @@
     describe('calculator tests', function () {
       describe('sum test', function () {
         it('should add all of the numbers in the array provided', function () {
-          let result = window.calc.sum([1]);
-          expect(result).to.equal(1);
+          let result = window.calc.sum([1,2,3]);
+          expect(result).to.equal(6);
+          expect(result).to.be.a('number');
+        });
+        it('should handle no arguments', function () {
+          let result = window.calc.sum();
+          expect(result).to.equal(0);
         });
       });
 
